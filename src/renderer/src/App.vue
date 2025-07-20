0a822fcbb0f68810977b1016628202fa899900f3
Sworld <61224072+mcthesw@users.noreply.github.com>
2025-07-21 00:27:55 +0800

    feat(ui): 集成Naive UI组件库并添加测试按钮
    - 添加naive-ui和vfonts依赖包
    - 在App.vue中引入NButton组件
    - 实现基础的按钮点击交互功能

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
     <HelloWorld msg="Hello World from Vue + Electron!" />
+    <NButton type="primary" @click="onSuccClick">S U C C</NButton>
   </div>
 </template>
 
 <script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { NButton } from 'naive-ui'
+
+const onSuccClick = (): void => {
+  alert('succ')
+}
 </script>
 
 <style>
